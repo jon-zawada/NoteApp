ea53f17ba323cd039cbdf2f4c904e16affc764d6
jon-zawada <jonathan_zawada@yahoo.com>
2023-08-05 18:45:59 -0700

    error handle for intializing localstorage if not previously set

--- src/useLocalStorage.ts
+++ src/useLocalStorage.ts
@@ -1,18 +1,20 @@
-import React from "react";
+import { useEffect, useState } from "react";
 
 export function useLocalStorage<T>(key: string, initialValue: T | (() => T)) {
-  const [value, setValue] = React.useState<T>(() => {
+  const [value, setValue] = useState<T>(() => {
     const jsonValue = localStorage.getItem(key);
     if (jsonValue == null) {
       if (typeof initialValue === "function") {
         return (initialValue as () => T)();
+      } else {
+        return initialValue;
       }
     } else {
       return JSON.parse(jsonValue);
     }
   });
 
-  React.useEffect(() => {
+  useEffect(() => {
     localStorage.setItem(key, JSON.stringify(value));
   }, [value, key]);
 
